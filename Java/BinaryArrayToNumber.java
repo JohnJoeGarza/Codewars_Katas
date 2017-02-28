11655f7a780c3ad9ab88a459a819328332bc60ae
John Garza <johnjoegarza@hotmail.com>
2017-02-27 17:58:45 -0800

    Create BinaryArrayToNumber.java

---
+++ Java/BinaryArrayToNumber.java
@@ -0,0 +1,36 @@
+/*Description:
+*
+*Given an array of one's and zero's convert the equivalent binary value to an integer.
+*
+*Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1
+*
+*Examples:
+*
+*Testing: [0, 0, 0, 1] ==> 1
+*Testing: [0, 0, 1, 0] ==> 2
+*Testing: [0, 1, 0, 1] ==> 5
+*Testing: [1, 0, 0, 1] ==> 9
+*Testing: [0, 0, 1, 0] ==> 2
+*Testing: [0, 1, 1, 0] ==> 6
+*Testing: [1, 1, 1, 1] ==> 15
+*Testing: [1, 0, 1, 1] ==> 11
+*/
+
+import java.util.List;
+import java.util.Arrays;
+
+public class BinaryArrayToNumber {
+
+    public static int ConvertBinaryArrayToInt(List<Integer> binary) {
+    List<Integer> binValues = Arrays.asList(8,4,2,1);
+    int listSize = binValues.size();
+    int sum = 0;
+    
+    for(int i = 0; i < listSize; i++){
+    sum += (binary.get(i) * binValues.get(i));
+    }
+    
+    return sum;
+    
+  }
+}
