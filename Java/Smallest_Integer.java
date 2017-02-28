5ade36f38424ab64ef5d976c21a4668b0b3de444
John Garza <johnjoegarza@hotmail.com>
2017-02-27 17:55:24 -0800

    Create Smallest_Integer.java

---
+++ Java/Smallest_Integer.java
@@ -0,0 +1,26 @@
+/*Description:
+*
+*Find the smallest integer in the array.
+*
+*Given an array of integers your solution should find the smallest integer. For example:
+*Given [34, 15, 88, 2] your solution will return 2
+*Given [34, -345, -1, 100] your solution will return -345
+*
+*You can assume, for the purpose of this kata, that the supplied array will not be empty.
+*/
+
+
+public class SmallestIntegerFinder {
+    public static int findSmallestInt(int[] args) {
+    
+    int currentSmallest = args[0];
+    int size = args.length;
+    
+    for(int i = 1; i < size; i++){
+      if(args[i] < currentSmallest){
+        currentSmallest = args[i];
+      }
+    }
+  return currentSmallest;
+ }
+}
